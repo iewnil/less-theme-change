86b9d68f20c2013392ed15112f2f3779c4ee6724
林伟 <lin.wei126@iwhalecloud.com>
2022-06-13 01:50:00 +0800

    fix: 插件path模块未引入

--- packages/less-theme-change-webpack-plugin/index.js
+++ packages/less-theme-change-webpack-plugin/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const webpack = require('webpack');
 const { bundleLessTheme, miniLessTheme, replaceLessTheme} = require('@lingxiteam/bundle-less-theme-tools');
 
